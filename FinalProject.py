917144fe683ab1c7fe536066b7781478ec8a82c8
Muna Salem <msa240002@utdallas.edu>
2026-04-21 12:57:54 -0500

    I created a resolution size and screen display function that is set to a 1024x1024 res. I chose this size due to the images I found for the background of the game.

--- FinalProject.py
+++ FinalProject.py
@@ -1,27 +1,45 @@
 import pygame
 import random
 
-# Create a screen size and display for game using pygame import.
-# Find images and use pygame to load them into game.
 
-# Map function: Will need to be a randomized grid with enemy and tresure spawns. 
+def main():
+    pygame.init()
 
-# Player will have a max hp and start off in a player position on the grid [0, 0].
+    # Create a screen size and display for game using pygame import.
+    size = 5
+    tile = 160
+    width = 1024
+    height = 1024
+    grid_size = size * tile
+    ui_height = height - grid_size
 
-# Combat function: Text based combat function. The only choices player has is attack or defend. Enemy and player has a random chance and random dmg output.
-#   Start combat system will randomize enemy stats
-#   Player attack will have a min and max hit chance using if else statements
-#   Enemy attack will have similar min max ratio but they can also dodge.
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.set_caption("Dungeon Crawler")
+    font = pygame.font.SysFont("Ariel", 36)
 
-# Win condition is killing 5 emenies. Add a "You won!" text.
+    # Find images and use pygame to load them into game.
 
-#A simple UI display showing player HP, Enemy HP, Messages, Kills.
+    # Map function: Will need to be a randomized grid with enemy and tresure spawns. 
 
-# Game loop using if else elif statements to keep the game going after combat is done.
-# Movement using arrow keys and if else elif statements. This will affect player position on the grid if not in combat.
-# Using up, down, and return keys during combat to select choices in combat.
+    # Player will have a max hp and start off in a player position on the grid [0, 0].
 
-# Respawn system for enemies and treasure? (maybe)
-# Lose condition if player health goes to 0, display "You died!" text.
+    # Combat function: Text based combat function. The only choices player has is attack or defend. Enemy and player has a random chance and random dmg output.
+    #   Start combat system will randomize enemy stats
+    #   Player attack will have a min and max hit chance using if else statements
+    #   Enemy attack will have similar min max ratio but they can also dodge.
 
-# Player "animation"? Player and enemy sprites flash if they take damage. Use blit and flash surface in images loaded in. 
+    # Win condition is killing 5 emenies. Add a "You won!" text.
+
+    #A simple UI display showing player HP, Enemy HP, Messages, Kills.
+
+    # Game loop using if else elif statements to keep the game going after combat is done.
+    # Movement using arrow keys and if else elif statements. This will affect player position on the grid if not in combat.
+    # Using up, down, and return keys during combat to select choices in combat.
+
+    # Respawn system for enemies and treasure? (maybe)
+    # Lose condition if player health goes to 0, display "You died!" text.
+
+    # Player "animation"? Player and enemy sprites flash if they take damage. Use blit and flash surface in images loaded in. 
+
+if __name__ == "__main__":
+    main()
